96db8e4bb0a35a6512747bfdc476ddef128d86a8
lcamilo <lcamilo@esec.com.br>
2016-08-02 22:54:36 -0300

    master: corrigindo import

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,14 +1,10 @@
-angular.module('minhasDiretivas',[]).direcitve('meuPainel',function(){
-	var ddo = {};
-	ddo.restrict = "AE";
-
-	ddo.scope =  {
-		titulo : '@titulo',
-	};
-
-	ddo.transclude = true;
-
-	ddo.templateUrl = 'js/directives/meu-panel.html'
-
-	return ddo;
+angular.module('minhasDirectivas',[]).directive('meuPainel',function(){
+  return {
+    restict:"AE",
+    scope : {
+      titulo : "@"
+    },
+    transclude: true,
+    templateUrl: "js/directives/meu-painel.html"
+  }
 });
