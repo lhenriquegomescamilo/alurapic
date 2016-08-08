3f7d7d2d3f81f7f20bb3437b4f3a213d7990a34f
lcamilo <lcamilo@esec.com.br>
2016-08-07 21:07:36 -0300

    master: Incluindo botao de remover

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -3,9 +3,23 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 
 	$scope.filtro = '';
 
-	$http.get('v1/fotos').success(function(fotos){
+	salvarFotos($scope,$http);
+
+	$scope.remover = function(foto){
+		$http.delete('/v1/fotos/' + foto._id)
+		.success(function(){
+			console.log('Foto '+foto.titulo+' foi removida com sucesso');
+		}).error(function(error){
+			console.log('Erro ao tentar remover '+error);
+		});
+	};
+
+});
+
+var salvarFotos = function($scope ,$http){
+	$http.get('/v1/fotos').success(function(fotos){
 		$scope.fotos = fotos;
 	}).error(function(error){
 		console.log("Erro encontrado"+erro);
 	});
-});
+}
